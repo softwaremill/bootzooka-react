efa4f7e6d37c6696c1b242826352b2657c49ef64
belfz <marcinbar1@gmail.com>
2018-07-12 15:40:46 +0200

    adds Welcome screen

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-// import { BrowserRouter as Router, Route, Redirect, Switch, Link } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import NavBar from './NavBar/NavBar';
+import Welcome from './Welcome/Welcome';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Welcome />
+          </Route>
+          <Route path="/home">
+            <p>home</p>
+          </Route>
+          <Route path="/login">
+            <p>login</p>
+          </Route>
+          <Route path="/register">
+            <p>register</p>
+          </Route>
+          <Route render={() => <Redirect to="/" />} />
+        </Switch>
       </div>
     );
   }
---
+++ ui/src/Welcome/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Welcome = () =>
+  <div className="Welcome">
+    <h1>Hi there! Welcome to Bootzooka-react!</h1>
+    <p>In this template application you can register as a new user, log in and later manage your user details.</p>
+    <p>If you are interested in how Bootzooka-react works, you can <a href="http://softwaremill.github.io/bootzooka/" target="blank">browse the documentation</a>, or the <a href="https://github.com/softwaremill/bootzooka-react" target="blank">source code</a>.</p>
+    <div className="Welcome__footer">
+      <h4>brought to you by</h4>
+      <a href="http://softwaremill.com" target="_blank"><img src="sml_2.png" className="sml-logo" alt="SoftwareMill"/></a>
+    </div>
+  </div>;
+
+export default Welcome;
